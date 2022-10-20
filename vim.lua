5330022d31a56f4b2efa7e52999e415bf43679a5
michaelbagnato <michaelbagnato@hotmail.com>
2022-10-21 00:05:49 +1100

    Convert set commands to vim.o settings

--- vim.lua
+++ vim.lua
@@ -2,26 +2,24 @@ package.path = package.path .. ";/home/michael/vimConfig/?.lua"
 require('plugins')
 require('treesitter')
 
+vim.o.number = true
+
+-- Three space tabs
+vim.o.tabstop = 3
+vim.o.shiftwidth = 3
+
+-- Smart tabs
+vim.o.smarttab = true
+
+vim.o.viminfo = false
+
+-- Terminal GUI colours
+vim.o.termguicolors = true
+
+
+vim.cmd('colorscheme dracula')
+
 vim.cmd([[
-colorscheme dracula 
-" Basic vim settings
-" ==============================================================================
-
-" Display line numbers
-set number
-
-" Three space tabs
-set tabstop=3
-set shiftwidth=3
-
-" Smart tabs
-set smarttab
-
-set viminfo=""
-
-" Terminal GUI colours
-set termguicolors
-
 source ~/vimConfig/lsp.vimrc
 source ~/vimConfig/keymappings.vimrc
 source ~/vimConfig/lualine.vimrc
