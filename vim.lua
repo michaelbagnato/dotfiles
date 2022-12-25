fcd2bbdf695ffbeca7aad9c3e63be05a65ebe061
Michael Bagnato <michaelbagnato@hotmail.com>
2022-12-25 20:19:57 +1100

    Adjust package path

--- vim.lua
+++ vim.lua
@@ -1,4 +1,4 @@
-package.path = package.path .. ";/home/michael/vimConfig/?.lua"
+package.path = package.path .. ";/home/michaelb/vimConfig/?.lua"
 require('plugins')
 require('statusline')
 require('treesitter')
