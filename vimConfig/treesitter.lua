1eeee2e2b0038371dc883f8cd1b6a067655107ec
Michael Bagnato <michaelbagnato@hotmail.com>
2023-02-13 21:28:36 +1100

    Addition of Go to treesitter

--- vimConfig/treesitter.lua
+++ vimConfig/treesitter.lua
@@ -1,5 +1,5 @@
 return require('nvim-treesitter.configs').setup {
-	ensure_installed = { "typescript", "vim", "lua", "c" },
+	ensure_installed = { "typescript", "vim", "lua", "go" },
 	sync_install = true,
 	auto_install = true,
 	highlight = {
