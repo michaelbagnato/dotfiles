d98a305573740454053da85e2166680b61cab8dc
Michael Bagnato <michaelbagnato@hotmail.com>
2023-02-26 13:18:30 +1100

    Addition of Dockerfile to Treesitter

--- vimConfig/treesitter.lua
+++ vimConfig/treesitter.lua
@@ -1,5 +1,5 @@
 return require('nvim-treesitter.configs').setup {
-	ensure_installed = { "typescript", "vim", "lua", "go" },
+	ensure_installed = { "typescript", "vim", "lua", "go", "dockerfile" },
 	sync_install = true,
 	auto_install = true,
 	highlight = {
