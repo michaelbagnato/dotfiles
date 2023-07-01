f99a89ed926c4cd3ed7912c4466701d665136545
michaelbagnato <michaelbagnato@hotmail.com>
2023-07-01 20:54:25 +1000

    Terminator config and script updates

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #! /usr/bin/zsh
 
+function installPackage {
+	packageName=$1
+	distro=$(cat /etc/*-release | head -1)
+	
+	if [ $distro = 'Manjaro Linux' ]; then
+		sudo pacman -Sy packageName
+	fi;
+}
+
+
 # Setup terminator
-# TODO Install Terminator
+installPackage terminator
 cd ~/.config
 ln -sv ~/dotfiles/terminator terminator
+
