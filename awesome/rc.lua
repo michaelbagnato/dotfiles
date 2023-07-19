483e96988ec931707921d7b05a18b4a02d4b2e5c
Michael Bagnato <michaelbagnato@hotmail.com>
2023-07-19 23:04:59 +1000

    Error handling module

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -22,30 +22,7 @@ local global = require("global")
 local bindings = require("keybindings")
 local widgets = require("widgets")
 
--- {{{ Error handling
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
-if awesome.startup_errors then
-    naughty.notify({ preset = naughty.config.presets.critical,
-                     title = "Oops, there were errors during startup!",
-                     text = awesome.startup_errors })
-end
-
--- Handle runtime errors after startup
-do
-    local in_error = false
-    awesome.connect_signal("debug::error", function (err)
-        -- Make sure we don't go into an endless error loop
-        if in_error then return end
-        in_error = true
-
-        naughty.notify({ preset = naughty.config.presets.critical,
-                         title = "Oops, an error happened!",
-                         text = tostring(err) })
-        in_error = false
-    end)
-end
--- }}}
+require("error-handling")
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
