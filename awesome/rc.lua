d0d0da911eec6a371a6a3e03eea5fa3f02044992
Michael Bagnato <michaelbagnato@hotmail.com>
2023-08-14 23:59:31 +1000

    Move default theme to repo; update battery font

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -23,7 +23,7 @@ require("error-handling")
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.init("/home/michael/.config/awesome/default/theme.lua")
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
--- awesome/widgets.lua
+++ awesome/widgets.lua
@@ -1,6 +1,7 @@
 local battery_widget = require("battery-widget") {
 	battery_prefix = utf8.char(128267),
-	ac_prefix = utf8.char(128268)
+	ac_prefix = utf8.char(128268),
+	widget_font = "JetBrainsMono Nerd Font 8"
 }
 
 return {
