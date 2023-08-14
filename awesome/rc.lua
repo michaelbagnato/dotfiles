ebf4b3c7b260e567c112d82a86c44ac22b1da47d
Michael Bagnato <michaelbagnato@hotmail.com>
2023-08-14 23:35:32 +1000

    Cleanup of unneeded items

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -11,7 +11,6 @@ local wibox = require("wibox")
 -- Theme handling library
 local beautiful = require("beautiful")
 
-local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
@@ -20,29 +19,7 @@ require("awful.hotkeys_popup.keys")
 local global = require("global")
 local bindings = require("keybindings")
 local widgets = require("widgets")
-
-local naughty = require("naughty")
-
-if awesome.startup_errors then
-    naughty.notify({ preset = naughty.config.presets.critical,
-                     title = "Oops, there were errors during startup!",
-                     text = awesome.startup_errors })
-end
-
--- Handle runtime errors after startup
-do
-    local in_error = false
-    awesome.connect_signal("debug::error", function (err)
-        -- Make sure we don't go into an endless error loop
-        if in_error then return end
-        in_error = true
-
-        naughty.notify({ preset = naughty.config.presets.critical,
-                         title = "Oops, an error happened!",
-                         text = tostring(err) })
-        in_error = false
-    end)
-end
+require("error-handling")
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
@@ -67,34 +44,6 @@ awful.layout.layouts = {
     -- awful.layout.suit.corner.sw,
     -- awful.layout.suit.corner.se,
 }
--- }}}
-
--- {{{ Menu
--- Create a launcher widget and a main menu
-myawesomemenu = {
-   { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
-   { "manual", global.terminal .. " -e man awesome" },
-   { "edit config", global.editor_cmd .. " " .. awesome.conffile },
-   { "restart", awesome.restart },
-   { "quit", function() awesome.quit() end },
-}
-
-internetMenu = {
-	{ "Chromium", "chromium"  },
-	{ "Thunderbird", "thunderbird" }
-}
-
-mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
-												{ "Internet", internetMenu },
-                                    { "open terminal", global.terminal }
-                                  }
-                        })
-
-mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
-                                     menu = mymainmenu })
-
--- Menubar configuration
-menubar.utils.terminal = global.terminal -- Set the terminal for applications that require it
 -- }}}
 
 -- Keyboard map indicator and switcher
@@ -195,7 +144,7 @@ awful.screen.connect_for_each_screen(function(s)
 				widgets.battery_widget,
 				-- TODO Include volumeicon
 				-- TODO Include weather
-            s.mytaglist,
+            --s.mytaglist,
             s.mypromptbox,
         },
         s.mytasklist, -- Middle widget
