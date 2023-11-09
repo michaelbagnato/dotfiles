24dc5fe3386ab6e7ad7c0004b4f33ac1a85f106c
Michael Bagnato <michaelbagnato@hotmail.com>
2023-11-09 16:50:20 +1100

    Addition of colour

--- awesome/widgets.lua
+++ awesome/widgets.lua
@@ -1,14 +1,19 @@
 local awful = require("awful")
 local wibox = require("wibox")
 
-local battery_command = "acpi"
-
 local battery_widget = wibox.widget {
 	{
-		widget = awful.widget.watch(battery_command, 10, function(widget, stdout)
+		widget = awful.widget.watch("acpi", 10, function(widget, stdout)
 			local output = string.match(stdout, "%d+%%")
-			widget:set_text("Battery: " .. output)
-		end),
+			local battery_level = tonumber(string.match(output, "%d+"))
+			if string.match(stdout, "Charging") and battery_level < 100 then
+				widget:set_markup("<span color='#00FF00'>" .. "Battery: " .. output .. "</span>")
+			elseif battery_level < 15 then
+				widget:set_markup("<span color='#FF0000'>" .. "Battery: " .. output .. "</span>")
+			else
+				widget:set_text("Battery: " .. output)
+			end
+		end)
 	},
 	layout = wibox.layout.fixed.horizontal,
 }
