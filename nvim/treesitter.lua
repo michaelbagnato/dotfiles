7b35f490d2f2d52bf420c0f28047185fdfe367cd
Michael Bagnato <michaelbagnato@hotmail.com>
2023-12-26 20:57:15 +1100

    Treesitter cleanup

--- nvim/treesitter.lua
+++ nvim/treesitter.lua
@@ -1,5 +1,5 @@
 return require('nvim-treesitter.configs').setup {
-	ensure_installed = { "typescript", "vim", "go", "dockerfile", "markdown" },
+	ensure_installed = { "typescript", "vim", "go", "dockerfile", "markdown", "tsx" },
 	sync_install = true,
 	auto_install = true,
 	highlight = {
