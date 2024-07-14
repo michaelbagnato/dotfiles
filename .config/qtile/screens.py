06e61b26ae9666a322c5f2542c992bf5edfb9c97
michaelbagnato <michaelbagnato@hotmail.com>
2024-07-14 19:10:05 +1000

    Screen update

--- .config/qtile/screens.py
+++ .config/qtile/screens.py
@@ -46,4 +46,45 @@ def get_screens():
                 margin=[10, 5, 5, 5],
             ),
         ),
+        Screen(
+            wallpaper="~/Wallpaper.png",
+            wallpaper_mode="stretch",
+            top=bar.Bar(
+                [
+                    widget.CurrentLayout(padding=10),
+                    widget.GroupBox(),
+                    widget.WindowName(
+                        scroll = True,
+                        scroll_clear = True
+                    ),
+                    widget.Spacer(),
+                    widget.Clock(format="%d/%m/%Y %a %I:%M %p"),
+                    widget.Spacer(),
+                    widget.Wttr(
+                        location={"Melbourne": "Melbourne"} 
+                    ),
+                    widget.Sep(),
+                    widget.BatteryIcon(),
+                    widget.Battery(
+                        format = "{percent:2.0%}",
+                        notify_below=10,
+                    ),
+                    widget.TextBox(
+                        text=f"{power_profile.stdout.decode().strip()}",
+                    ),
+                    widget.Sep(),
+                    widget.Volume(
+                        device='pipewire',
+                        fmt='🔊 {}',
+                        mute_format='🔇'
+                    ),
+                    widget.Sep(),
+                    widget.Systray()
+                ],
+                32,
+                #background="#570296",
+                background="#3c345c",
+                margin=[10, 5, 5, 5],
+            )
+        )
     ]
