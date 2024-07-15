9c3a8aaafef34941a88fcd5484e1bcc174c16f71
michaelbagnato <michaelbagnato@hotmail.com>
2024-07-15 20:26:33 +1000

    UEFI alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,10 @@
-fish_config theme choose "Dracula"
+# Disables the greeting message
 set -U fish_greeting
+
+# Sets the theme
+fish_config theme choose "Dracula"
+
+# Aliases
 alias upgrade="yay -Syu"
 alias %="fg"
-
+alias uefi-update="fwupdmgr refresh --force && fwupdmgr update"
