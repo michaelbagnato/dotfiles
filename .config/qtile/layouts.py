8542fb5a0149e2c51dfd60fad7f724beb59acee7
michaelbagnato <michaelbagnato@hotmail.com>
2024-11-20 18:15:32 +1100

    Layout update

--- .config/qtile/layouts.py
+++ .config/qtile/layouts.py
@@ -9,7 +9,12 @@ def get_layouts():
             margin=5,
             margin_on_single=10
         ),
-        layout.Max(margin=10),
+        layout.Max(
+            margin=10,
+            border_focus='#d30cb8',
+            border_normal='#3c345c',
+            border_width=2
+        ),
         layout.Floating(margin=10),
         # Try more layouts by unleashing below layouts.
         # layout.Stack(num_stacks=2),
