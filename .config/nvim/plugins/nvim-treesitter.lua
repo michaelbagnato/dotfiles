1e23285c2c81c9f8d0a1da85dfdc8cbaf57e21e1
michaelbagnato <michaelbagnato@hotmail.com>
2025-02-28 20:09:28 +1100

    Treesitter plugin

---
+++ .config/nvim/plugins/nvim-treesitter.lua
@@ -0,0 +1,17 @@
+vim.filetype.add({
+   pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
+})
+
+return {
+   "nvim-treesitter/nvim-treesitter",
+   build = ":TSUpdate",
+
+   config = function()
+      local configs = require("nvim-treesitter.configs")
+      configs.setup({
+         ensure_installed = { "hyprlang", "lua", "typescript" },
+         highlight = { enable = true },
+         indent = { enable = true },
+      })
+   end,
+}
