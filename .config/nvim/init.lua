890b4dd573ac815ebee3beec3138158f23e5c134
michaelbagnato <michaelbagnato@hotmail.com>
2025-03-30 17:49:55 +1100

    Split LSP into multiple files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,6 @@
 package.path = os.getenv("HOME") .. "/.config/nvim/?.lua;" .. package.path
 require("keybindings")
 require("plugins")
-require("lsp")
 
 -- Disable netrw
 vim.g.loaded_netrw = 1
@@ -17,3 +16,9 @@ vim.opt.expandtab = true
 -- Clear registers on load
 vim.opt.shada = nil
 
+--[[ 
+Start LSPs
+(Make sure they're installed on your system first and a config file
+has been created in the lsp folder)
+]]
+vim.lsp.enable({"lua_ls", "ts_ls"})
---
+++ .config/nvim/lsp/ts_ls.lua
@@ -0,0 +1,5 @@
+return {
+   cmd = { "typescript-language-server", "--stdio" },
+   root_markers = { ".git", "tsconfig.json" },
+   filetypes = { "typescript", "javascript" },
+}
