3e7e856e95e574ecff8b57356fdbcc38035d72d1
michaelbagnato <michaelbagnato@hotmail.com>
2025-05-20 21:36:07 +1000

    Neovim keybinding update

--- .config/nvim/keybindings.lua
+++ .config/nvim/keybindings.lua
@@ -8,7 +8,7 @@ keybind('<C-E>', '<cmd>NvimTreeToggle<CR>')
 -- Open telescope for file search
 keybind('<C-F>', '<cmd>Telescope find_files<CR>')
 
--- Show LSP errors in a popup
-keybind('<C-W>', function()
+-- Show LSP error currently under cursor
+keybind('<C-M>', function()
    vim.diagnostic.open_float(nil, { focusable = false })
 end)
