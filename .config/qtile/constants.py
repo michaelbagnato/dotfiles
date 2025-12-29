eebb78db7c11fe8fadd55845b384586b50d76964
Michael Bagnato <michaelbagnato@hotmail.com>
2025-12-29 17:21:33 +1100

    Remove Qtile

--- .config/qtile/constants.py
+++
@@ -1,8 +0,0 @@
-from qtile.utils import guess_terminal
-mod = "mod4"
-terminal = "ghostty"
-screenshot = "grim - | wl-copy"
-browser = "firefox"
-screenlock = "swaylock"
-runner = "rofi -icon-theme neo-candy-icons -show-icons -show drun"
-switcher = "rofi -icon-theme neo-candy-icons -show-icons -show window"
