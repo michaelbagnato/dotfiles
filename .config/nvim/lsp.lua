4b0b6f96da9270dba20af235f53f46012b713c18
Michael Bagnato <michaelbagnato@hotmail.com>
2026-01-07 23:20:00 +1100

    Pyright LSP

--- .config/nvim/lsp.lua
+++ .config/nvim/lsp.lua
@@ -3,5 +3,5 @@ Start LSPs
 (Make sure they're installed on your system first and a config file
 has been created in the lsp folder)
 ]]
-vim.lsp.enable({"lua_ls", "ts_ls", "gopls", "dartls"})
+vim.lsp.enable({"lua_ls", "ts_ls", "gopls", "dartls", "pyright"})
 
