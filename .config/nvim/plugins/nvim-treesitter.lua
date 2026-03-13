2cdc29a266ae5b2c77b68c79f60d5fee5a541996
Michael Bagnato <michaelbagnato@hotmail.com>
2026-03-14 00:05:05 +1100

    Add Dockerfiles and KDL to Treesitter

--- .config/nvim/plugins/nvim-treesitter.lua
+++ .config/nvim/plugins/nvim-treesitter.lua
@@ -5,7 +5,7 @@ return {
    config = function()
       local configs = require("nvim-treesitter.configs")
       configs.setup({
-         ensure_installed = {"lua", "typescript", "go", "dart", "python" },
+         ensure_installed = {"lua", "typescript", "go", "dart", "python", "dockerfile", "kdl" },
          highlight = { enable = true },
          indent = { enable = true },
       })
